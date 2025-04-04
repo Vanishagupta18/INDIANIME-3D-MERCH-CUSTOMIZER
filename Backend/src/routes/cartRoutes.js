091a66b59012059181b4fa6e02bb4ab73f29039b
Vanishagupta18 <guptavanisha11@gmail.com>
2025-04-04 14:00:00 +0530

    feat: add wishlist toggle endpoint

---
+++ Backend/src/routes/cartRoutes.js
@@ -0,0 +1,21 @@
+﻿import express from 'express'
+import asyncHandler from 'express-async-handler'
+import User from '../models/User.js'
+import { protect } from '../middleware/authMiddleware.js'
+
+const router = express.Router()
+
+// Wishlist toggle
+router.post('/wishlist/:productId', protect, asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+  const idx = user.wishlist.indexOf(req.params.productId)
+  if (idx === -1) {
+    user.wishlist.push(req.params.productId)
+  } else {
+    user.wishlist.splice(idx, 1)
+  }
+  await user.save()
+  res.json({ success: true, wishlist: user.wishlist })
+}))
+
+export default router
