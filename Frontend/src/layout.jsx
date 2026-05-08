690a4b1f5ee6e80eb77cbbb57bcc523a3c0f15e3
Vanishagupta18 <guptavanisha11@gmail.com>
2026-05-08 21:42:38 +0530

    fix: correct import case for Header and Footer components in Layout

--- Frontend/src/layout.jsx
+++ Frontend/src/layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
+import Header from './components/Header/header'
+import Footer from './components/Footer/footer'
 import { Toast } from './components/ui/Toast'
 
 export default function Layout() {
